b37b40a08f225c4f44385cf9916506e3652b16a0
Sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2022-03-20 21:46:59 +0900

    delete code

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,28 +1,12 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
-export const Test = (props) => {
+export const Test = () => {
   const propsState = useSelector((state) => state);
-
-  let data = "123";
-  let key = "";
-
-  const removeData = (data) => {
-    const change = "";
-
-    change = data.replaceAll("3", "");
-    return change;
-  };
-
-  const getKey = (data) => {
-    key = removeData(data);
-
-    return key;
-  };
 
   return (
     <div>
-      <button onClick={() => getKey(data)}>{key}</button>
+      <span>{propsState.name}</span>
     </div>
   );
 };
