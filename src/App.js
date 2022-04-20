6c08aa54b63376ab817df42547c43473a6ee9a53
sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2022-04-20 11:46:07 +0900

    Ckeditor test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { withRouter } from "react-router-dom";
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-function App(props) {
-  const getState = useSelector((state) => state);
-  const dispatch = useDispatch();
+function App() {
+  const [data, setData] = useState({
+    title: "",
+    content: "",
+  });
 
-  const { id, pwd, unique_id } = getState.member_info ? getState.member_info : getState;
+  console.log(data);
+
+  const onChange = (event, editor) => {
+    const editorData = editor.getData();
+
+    setData({
+      ...data,
+      content: editorData,
+    });
+  };
+
+  const onChangeOtherValue = ({ target }) => {
+    const { name, value } = target;
+
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
 
   return (
     <div>
-      <p>로그인 버튼을 눌러 아이디를 받으세요.</p>
-      {getState.member_info ? id + " " + pwd + " " + unique_id : "익명"}
-      <h1>{getState.number}</h1>
-      <button onClick={() => dispatch({ type: "로그인" })}>로그인</button>
-      <button onClick={() => dispatch({ type: "증가" })}>증가</button>
-      <button onClick={() => dispatch({ type: "감소" })}>감소</button>
-      <button onClick={props.location.pathname === "/move" ? () => props.history.push("/") : () => props.history.push("/move")}>이동</button>
-      <span>현재 URL: {props.location.pathname}</span>
+      <input type={"text"} name={"title"} onChange={onChangeOtherValue} />
+      <CKEditor name={"content"} editor={ClassicEditor} onChange={onChange} />
     </div>
   );
 }
 
-export default withRouter(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
 import App from "./App";
-import { Test } from "./components/Test";
 
-const initialState = {
-  id: 1,
-  name: "name",
-  number: 4,
-};
-
-const reducer = (state = initialState, action) => {
-  let number = 0;
-  let member_info = {};
-
-  switch (action.type) {
-    case "증가":
-      number = state.number + 1;
-      return { ...state, number };
-    case "감소":
-      number = state.number - 1;
-      return { ...state, number };
-    case "로그인":
-      member_info = {
-        id: "jiwooproity",
-        pwd: "023thwldndkdlel!",
-        unique_id: 1,
-      };
-      return {
-        ...state,
-        member_info,
-      };
-    default:
-      return state;
-  }
-};
-
-let store = createStore(reducer);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-        <Test />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
